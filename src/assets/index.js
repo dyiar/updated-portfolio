cc3cb7d032e2fc7d5e7df5ef89decb2a0329f9a2
dyiar <austin.hl.blake@gmail.com>
2023-08-26 19:39:43 -0700

    fixed build error

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,7 +13,6 @@ import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
@@ -21,14 +20,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import tv from "./tv.png"
 import moodbot from "./moodbot.png"
 import story from "./story.png"
@@ -49,20 +40,12 @@ export {
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
   redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   tv,
   moodbot,
   story,
